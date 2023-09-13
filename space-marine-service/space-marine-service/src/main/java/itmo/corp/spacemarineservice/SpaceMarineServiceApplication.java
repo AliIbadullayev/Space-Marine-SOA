0ce770e8d86700cc1d47882ac63c4deb31656153
va.v.osipov <va.v.osipov@tinkoff.ru>
2023-09-13 21:22:35 +0300

    initial commit

---
+++ space-marine-service/space-marine-service/src/main/java/itmo/corp/spacemarineservice/SpaceMarineServiceApplication.java
@@ -0,0 +1,13 @@
+package itmo.corp.spacemarineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceMarineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceMarineServiceApplication.class, args);
+    }
+
+}
---
+++ space-marine-service/space-marine-service/src/test/java/itmo/corp/spacemarineservice/SpaceMarineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.corp.spacemarineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceMarineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
